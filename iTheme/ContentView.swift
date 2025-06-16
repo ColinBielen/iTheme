7caf2ac813cf22a4936981281873f20b68b28a68
ColinB <colin.bielen@gmail.com>
2025-06-16 13:48:57 -0700

    Initial Commit

--- iTheme/ContentView.swift
+++ iTheme/ContentView.swift
@@ -1,24 +1,52 @@
-//
-//  ContentView.swift
-//  iTheme
-//
-//  Created by Colin on 6/16/25.
-//
-
 import SwiftUI
+import AVFoundation
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+    @State private var audioPlayer: AVAudioPlayer?
 
-#Preview {
-    ContentView()
+    var body: some View {
+        ZStack {
+            Image("bhc")
+                .resizable()
+                .scaledToFill()
+                .ignoresSafeArea()
+            VStack(spacing: 20) {
+                Button(action: {
+                    self.playSound()
+                }) {
+                    Text("Play Beverly Hills Cop Theme")
+                        .padding()
+                        .background(Color.blue.opacity(0.8))
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+                Button(action: {
+                    self.stopSound()
+                }) {
+                    Text("Stop")
+                        .padding()
+                        .background(Color.red.opacity(0.8))
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+            }
+        }
+    }
+
+    func playSound() {
+        if let url = Bundle.main.url(forResource: "axel_f", withExtension: "mp3") {
+            do {
+                audioPlayer = try AVAudioPlayer(contentsOf: url)
+                audioPlayer?.play()
+            } catch {
+                print("Error playing sound: \(error.localizedDescription)")
+            }
+        } else {
+            print("Audio file not found")
+        }
+    }
+
+    func stopSound() {
+        audioPlayer?.stop()
+    }
 }
