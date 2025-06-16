7c745ae70e0e3982554a4d22cdafa51788dd1561
ColinB <colin.bielen@gmail.com>
2025-06-16 12:47:27 -0700

    Initial Commit

---
+++ iTheme/iThemeApp.swift
@@ -0,0 +1,17 @@
+//
+//  iThemeApp.swift
+//  iTheme
+//
+//  Created by Colin on 6/16/25.
+//
+
+import SwiftUI
+
+@main
+struct iThemeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iThemeTests/iThemeTests.swift
@@ -0,0 +1,17 @@
+//
+//  iThemeTests.swift
+//  iThemeTests
+//
+//  Created by Colin on 6/16/25.
+//
+
+import Testing
+@testable import iTheme
+
+struct iThemeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
